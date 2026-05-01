12079e594c23f303af2d038a261b02da75612549
robertjantyik <56860708+robertjantyik@users.noreply.github.com>
2026-05-01 22:39:56 +0200

    db connection update

--- backend/db.php
+++ backend/db.php
@@ -1,9 +1,9 @@
 <?php
 
 $dbh = new PDO(
-    'mysql:host=localhost;dbname=adatb;charset=utf8mb4',
-    'adatbf',
-    'JELSZO_HELYE',
+    'mysql:host=localhost;dbname=webprognje',
+    'webprognje',
+    'WebProgNJE123',
     array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
 );
 
